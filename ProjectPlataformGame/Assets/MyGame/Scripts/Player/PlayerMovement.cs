742879faa91ef1be7a2942eff4555fa64e540df8
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2023-05-09 17:58:28 -0300

    Adding player animation
    Adding player animation script and the method call inside the player movement script to make it execute the movement animation.

--- ProjectPlataformGame/Assets/MyGame/Scripts/Player/PlayerMovement.cs
+++ ProjectPlataformGame/Assets/MyGame/Scripts/Player/PlayerMovement.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour,IPlayerMovement
 {
+    private PlayerAnimation playerAnimation;
+
+    private void Awake()
+    {
+        playerAnimation = GetComponent<PlayerAnimation>();
+    }
+
     public void Move(Vector3 direction, float speed)
     {
         transform.position += direction * speed * Time.deltaTime;
 
         if (direction.x > 0)
         {
+            playerAnimation.SetAnimation(1);
             transform.eulerAngles = new Vector3(0, 0, 0);
-
-        }else if(direction.x < 0)
-        {
             
+        }
+        else if(direction.x < 0)
+        {
+            playerAnimation.SetAnimation(1);
             transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            playerAnimation.SetAnimation(0);
         }
     }
 
