2e9557409325cf4598337cc154b4c717f82b376a
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2023-05-30 10:53:35 -0300

    Adding restart and quit game
    When the player dies the restart and exit buttons appear on the screen.

--- ProjectPlataformGame/Assets/MyGame/Scripts/GameController/GameController.cs
+++ ProjectPlataformGame/Assets/MyGame/Scripts/GameController/GameController.cs
@@ -7,7 +7,8 @@ public class GameController : MonoBehaviour
     public static GameController instance;
 
     public PlayerAnimation playerAnimation;
- 
+
+    public GameObject gameOverScreen;
     void Awake()
     {
         instance = this;
@@ -26,6 +27,18 @@ public class GameController : MonoBehaviour
         playerAnimation.GetComponent<Transform>().position -= Vector3.down * 2 * Time.deltaTime;
         playerAnimation.GetComponent<Collider2D>().enabled = false;
         yield return new WaitForSeconds(2);
+        gameOverScreen.SetActive(true);
+        
+    }
+
+    public void RestartGame()
+    {
+        gameOverScreen.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
