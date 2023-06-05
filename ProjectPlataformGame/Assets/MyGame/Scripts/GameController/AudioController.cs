8f0527cacfdf8f7e934f7dd05308e301e16752f8
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2023-06-05 12:31:04 -0300

    Adding audio controller
    The audio controller will manage the audio system.

---
+++ ProjectPlataformGame/Assets/MyGame/Scripts/GameController/AudioController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController instance;
+
+    public List<AudioClip>songs = new List<AudioClip> ();
+
+    public AudioSource audioSource;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void PlaySong(AudioClip audio)
+    {
+        audioSource.PlayOneShot (audio);
+    }
+}
