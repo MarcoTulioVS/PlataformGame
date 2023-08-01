86cd89f66afb9844d4465b3597b5ac7f59b02177
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2023-08-01 19:30:40 -0300

    Adding player movement in the scene intro
    The player moves forward to a certain point and stops.

--- ProjectPlataformGame/Assets/MyGame/Scripts/Intro/Player/PlayerIntro.cs
+++ ProjectPlataformGame/Assets/MyGame/Scripts/Intro/Player/PlayerIntro.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerIntro : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed;
+
+    private bool isStopped;
+
+    private Animator anim;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Move();
+
+        if (isStopped)
+        {
+            Stop();
+        }
     }
+
+    private void Move()
+    {
+        rb.velocity = new Vector3(-speed, rb.velocity.y);
+    }
+
+    private void Stop()
+    {
+        rb.velocity = Vector3.zero;
+        anim.SetInteger("transition", 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "stopArea")
+        {
+            isStopped = true;
+        }
+    }
+
+    
 }
